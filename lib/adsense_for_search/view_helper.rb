f79d2bbd764a7219414b37dba63f0c34f2b1e587
Cassia Catherine Monteiro <kssia.cm@gmail.com>
2012-04-16 14:16:41 -0300

    cut adblock 3 from the js helper

--- lib/adsense_for_search/view_helper.rb
+++ lib/adsense_for_search/view_helper.rb
@@ -15,14 +15,12 @@ module AdSenseForSearch
       
       variables = ['pageOptions']
       variables << 'adblock1' if configurations[:adblock1]
-      variables << 'adblock2' if configurations[:adblock2]
-      variables << 'adblock3' if configurations[:adblock3]
+      variables << 'adblock2' if configurations[:adblock2]      
       
       js = ""
       js << "var pageOptions = #{ configurations[:pageOptions].to_json2 };\n"
       js << "\n  var adblock1 = #{ configurations[:adblock1].to_json2 };\n" if configurations[:adblock1]
       js << "\n  var adblock2 = #{ configurations[:adblock2].to_json2 };\n" if configurations[:adblock2]
-      js << "\n  var adblock3 = #{ configurations[:adblock3].to_json2 };\n" if configurations[:adblock3]
       js << "\n  new google.ads.search.Ads(#{variables.join(', ')});"
       
       output = <<-javascript
